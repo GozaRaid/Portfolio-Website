c13a2d14898660468747b3fe661020cd549f42db
GozaRaid <hafidzhitobaihaqi@gmail.com>
2025-01-26 00:15:30 +0700

    style: restructure code based on prettier

--- main.js
+++ main.js
@@ -1,61 +1,60 @@
-window.onload = function() {
+window.onload = function () {
   window.scrollTo(0, 0);
-}
+};
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   window.location.reload(false);
-}
+};
 
 //FadeinSection
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fadeInSections();
-  window.addEventListener('scroll', fadeInSections);
-  window.addEventListener('resize', fadeInSections);
+  window.addEventListener("scroll", fadeInSections);
+  window.addEventListener("resize", fadeInSections);
 });
 
 function fadeInSections() {
-  const sections = document.querySelectorAll('.to-fade-in');
-  
-  sections.forEach(section => {
+  const sections = document.querySelectorAll(".to-fade-in");
+
+  sections.forEach((section) => {
     const sectionTop = section.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
-    
+
     if (sectionTop < windowHeight) {
-      section.classList.add('is-visible');
+      section.classList.add("is-visible");
     } else {
-      section.classList.remove('is-visible');
+      section.classList.remove("is-visible");
     }
   });
 }
 
 //Navbar effect
-document.addEventListener("DOMContentLoaded",function(){
-  var navbar = document.getElementById("navbar")
-  window.addEventListener("scroll", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+  window.addEventListener("scroll", function () {
     if (window.scrollY) {
       navbar.classList.add("on-scroll");
     } else {
       navbar.classList.remove("on-scroll");
     }
   });
-  
-  var menu_btn = document.querySelector('.hamburger-lines');
-	var mobile_menu = document.querySelector('.nav-mobile');
 
-	menu_btn.addEventListener('click', function () {
+  var menu_btn = document.querySelector(".hamburger-lines");
+  var mobile_menu = document.querySelector(".nav-mobile");
+
+  menu_btn.addEventListener("click", function () {
     mobile_menu.classList.add("on-scroll");
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-	});
-
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
 });
 
 //scroll-behavior
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var navbarHeight = document.getElementById("navbar").offsetHeight;
   var links = document.querySelectorAll("#navbar div a");
-  links.forEach(function(link) {
-    link.addEventListener("click", function(event) {
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
       var targetId = this.getAttribute("href").substring(1);
       var targetElement = document.getElementById(targetId);
@@ -63,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var targetPosition = targetElement.offsetTop - navbarHeight;
         window.scrollTo({
           top: targetPosition,
-          behavior: "smooth"
+          behavior: "smooth",
         });
       }
     });
@@ -71,19 +70,19 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //animated-percentage
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Select the elements with class "percentage"
-  var elements = document.querySelectorAll('.percentage');
+  var elements = document.querySelectorAll(".percentage");
 
   // Function to handle intersection of the target element with the viewport
   function handleIntersection(entries, observer) {
-    entries.forEach(function(entry) {
+    entries.forEach(function (entry) {
       // Check if the target element is intersecting with the viewport
       if (entry.isIntersecting) {
         var targetNumber = parseInt(entry.target.textContent);
         var currentNumber = 0;
 
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
           currentNumber += Math.ceil(targetNumber / 50);
           if (currentNumber >= targetNumber) {
             currentNumber = targetNumber;
@@ -97,46 +96,49 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
   // Create a new IntersectionObserver instance
-  var observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+  var observer = new IntersectionObserver(handleIntersection, {
+    threshold: 0.5,
+  });
   // Observe each element with class "percentage"
-  elements.forEach(function(element) {
+  elements.forEach(function (element) {
     observer.observe(element);
   });
 });
 
-
 // Contact form
-var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
-script.onload = function() {
+var script = document.createElement("script");
+script.src =
+  "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
+script.onload = function () {
   emailjs.init({
-    publicKey: "YEf6kqse4-p32OAkU"
+    publicKey: "YEf6kqse4-p32OAkU",
   });
 };
 document.head.appendChild(script);
 
 var button = document.getElementById("sendmail");
-button.addEventListener('click', function (event) {
+button.addEventListener("click", function (event) {
   event.preventDefault();
   SendMail();
-})
+});
 
-function SendMail(){
+function SendMail() {
   var parms = {
-    from_name : document.getElementById("name").value.trim(),
-    email_id : document.getElementById("email").value.trim(),
-    message : document.getElementById("message").value.trim()
-  }
-  
-  if(!parms.from_name || !parms.email_id || !parms.message){
+    from_name: document.getElementById("name").value.trim(),
+    email_id: document.getElementById("email").value.trim(),
+    message: document.getElementById("message").value.trim(),
+  };
+
+  if (!parms.from_name || !parms.email_id || !parms.message) {
     alert("Please fill out all fields");
     return;
   } else {
-    emailjs.send("service_v1a7gu5","template_c4fkpk8",parms)
-    .then(alert("Email sent successfully!"))
-    .catch(function(err){
-      alert("Failed to send email: " + err);
-    });
+    emailjs
+      .send("service_v1a7gu5", "template_c4fkpk8", parms)
+      .then(alert("Email sent successfully!"))
+      .catch(function (err) {
+        alert("Failed to send email: " + err);
+      });
   }
 }
 
